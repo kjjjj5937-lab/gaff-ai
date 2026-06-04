97fc345b5566965dcd98f53052e1f0f27d75d135
kjjjj5937-lab <kjjjj5937@gmail.com>
2026-06-04 13:42:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212666666666;
+global.pairingNumber = 212677427109;
 global.owner = [
-  ['212717457920', 'noureddine Ouafy', true],
-  ['', 'Owner 2', true],
+  ['', 'noureddine Ouafy', true],
+ 212677427109 ['', 'Owner 2', true],
 ];
 
 global.namebot = 'gaff ai';
